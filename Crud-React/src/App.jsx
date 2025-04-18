fc07b7ede7ec33d3921487dc9dab9102ee587a24
carlosmorales274135 <carlosmorales274135@gmail.com>
2025-04-18 20:20:16 +0000

    Todo el inicio hasta la tabla

---
+++ Crud-React/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import TableList from './components/TableList'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <h1>hola mundo</h1> 
+    <TableList />
+    </>
+  )
+}
+
+export default App
---
+++ Crud-React/src/components/TableList.jsx
@@ -0,0 +1,46 @@
+export default function TableList() {
+
+    const clients=[
+        {id:1,name:"John Doe", email:"John.Doe@gmail.com", job:"Developer", rate:"100", isactive:true},
+        {id:2,name:"John Doe", email:"John.Doe@gmail.com", job:"Developer", rate:"100", isactive:false},
+        {id:3,name:"John Doe", email:"John.Doe@gmail.com", job:"Developer", rate:"100", isactive:true},        
+    ];
+
+
+
+    return(
+        <>
+        <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>email</th>
+        <th>Job</th>
+        <th>rate</th>
+        <th>status</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      {clients.map((client)=>(
+        <tr>
+        <th>{client.id}</th>
+        <td>{client.name}</td>
+        <td>{client.email}</td>
+        <td>{client.job}</td>
+        <td>{client.rate}</td>
+        <td>
+{/* Suggested code may be subject to a license. Learn more: ~LicenseLog:2652040894. */}
+            <button className="btn btn-xs ${client.isactive ? btn-success : btn-error}">Hola Mundo</button>
+        </td>
+      </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+        </>
+    );
+}
